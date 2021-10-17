7c3483ac34d0e3c9786f5d0cb65027171e58cfa3
Amara <amar_algu@hotmail.com>
2021-10-17 07:30:35 -0400

    idex file corrected, prompt question and answer working

--- index.js
+++ index.js
@@ -12,21 +12,121 @@ function mainMenu(){
         {
             type: "list",
             name: "memberChoice",
-            message: "Who would you like to do?",
+            message: "Who would you like to add?",
             choices: ["Add Engineer", "Add Intern,", "Add Manager", "Quit"]
         }
     ]).then(answers =>{
-        if(answers.memberChoice === "Add Engineer"){ 
-            function createEngineer()      
-             console.log (createEngineer)
+        if(answers.memberChoice === "Add Engineer"){ function createEngineer(){
+
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "engineerName",
+                    message: "What is the team engineer's name?"
+                },
+                {
+                    type: "input",
+                    name: "engineerId",
+                    message: "What is the team engineer's Id?"
+                },
+                {
+                    type: "input",
+                    name: "engineerEmail",
+                    message: "What is the team engineer's email?"
+                },
+                {
+                    type: "input",
+                    name: "engineerGithub",
+                    message: "What is the team engineer's Github?"
+                },
+            ]).then(answers =>{
+                const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.Github)
+                team.push(engineer)
+                console.log("team", team)
+                console.log(engineer)
+                mainMenu()
+            }) 
+        }
+        createEngineer()
+             
+             
                 // fire off the function to create engineer
             }if(answers.memberChoice === "Add Intern,")
-            { createIntern()
-                console.log(createIntern)
+            { function createIntern(){
+
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "internName",
+                        message: "What is the team intern's name?"
+                    },
+                    {
+                        type: "input",
+                        name: "internId",
+                        message: "What is the team intern's Id?"
+                    },
+                    {
+                        type: "input",
+                        name: "internEmail",
+                        message: "What is the team intern's email?"
+                    },
+                    {
+                        type: "input",
+                        name: "internSchool",
+                        message: "What is the team intern's school?"
+                    },
+                ]).then(answers =>{
+                    const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
+                    team.push(intern)
+                    console.log("team", team)
+                    console.log(intern)
+                    mainMenu()
+                
+                   
+            })
+        }createIntern()
+                
 
             }if (answers.memberChoice === "Add Manager") {
+                function createManager(){
+
+                    inquirer.prompt([
+                        {
+                            type: "input",
+                            name: "managerName",
+                            message: "What is the team manager's name?"
+                        },
+                        {
+                            type: "input",
+                            name: "managerId",
+                            message: "What is the team manager's Id?"
+                        },
+                        {
+                            type: "input",
+                            name: "managerEmail",
+                            message: "What is the team manager's email?"
+                        },
+                        {
+                            type: "input",
+                            name: "managerOfficeNumber",
+                            message: "What is the team manager's office number?"
+                        },
+                    ]).then(answers =>{
+                        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
+                        team.push(manager)
+                        console.log("team", team)
+                        console.log(manager)
+                        mainMenu()
+                    
+                      
+                          
+            
+                    })
+                    
+                }
+                
                 createManager()
-                console.log(createManager)
+               
             } else (answers.memberChoice === "Quit") 
             createTeam
         })   
@@ -39,115 +139,17 @@ function mainMenu(){
    
 function createTeam(){
 
-    function createManager(){
-
-        inquirer.prompt([
-            {
-                type: "input",
-                name: "managerName",
-                message: "What is the team manager's name?"
-            },
-            {
-                type: "input",
-                name: "managerId",
-                message: "What is the team manager's Id?"
-            },
-            {
-                type: "input",
-                name: "managerEmail",
-                message: "What is the team manager's email?"
-            },
-            {
-                type: "input",
-                name: "managerOfficeNumber",
-                message: "What is the team manager's office number?"
-            },
-        ]).then(answers =>{
-            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
-            team.push(manager)
-            console.log("team", team)
-            console.log(manager)
-            mainMenu()
-        
-          
-              
-
-        })
-        
-    }
-    createManager()
+   
 
     
 } 
 
 
-function createEngineer(){
-
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "engineerName",
-            message: "What is the team engineer's name?"
-        },
-        {
-            type: "input",
-            name: "engineerId",
-            message: "What is the team engineer's Id?"
-        },
-        {
-            type: "input",
-            name: "engineerEmail",
-            message: "What is the team engineer's email?"
-        },
-        {
-            type: "input",
-            name: "engineerGithub",
-            message: "What is the team engineer's github username?"
-        },
-    ]).then(answers =>{
-        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.github)
-        team.push(engineer)
-        console.log("team", team)
-        console.log(engineer)
-        mainMenu()
-    }) 
-}
-createEngineer()
 
 
 
-function createIntern(){
 
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "internName",
-            message: "What is the team intern's name?"
-        },
-        {
-            type: "input",
-            name: "internId",
-            message: "What is the team intern's Id?"
-        },
-        {
-            type: "input",
-            name: "internEmail",
-            message: "What is the team intern's email?"
-        },
-        {
-            type: "input",
-            name: "internSchool",
-            message: "What is the team intern's school name?"
-        },
-    ]).then(answers =>{
-        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
-        team.push(intern)
-        console.log("team", team)
-        console.log(intern)
-        mainMenu()
-    })
-}   
-createIntern()
+
     
             
 
