b787c4812e93bf0453050db30a87e7366b4e470c
Amara <amar_algu@hotmail.com>
2021-10-20 05:21:01 -0400

    html added, no link yet with index,js

--- index.js
+++ index.js
@@ -5,6 +5,55 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const team = [];
 
+const generateHTML = (answers) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Team Generator</title>
+</head>
+<body>
+  <header class="bg-info jumbotron jumbotron-fluid " style="--bs-bg-opacity: .5;">
+  <div class="container">
+    <h1 class="display-4 text-center " >My Team</h1>
+  </header>
+  
+  <div class="container bg-light">
+  <div class="row align-items-start">
+    <div class="col">
+      Managers Profiles
+      <ul class="list-group">
+      <li class="list-group-item">Name: ${answers.managerName}</li>
+      <li class="list-group-item">ID: ${answers.managerId}</li>
+      <li class="list-group-item">Email: ${answers.managerEmail}</li>
+      <li class="list-group-item">Phone number: ${answers.managerOfficeNumber}</li>
+    </ul>
+    </div>
+    <div class="col">
+      Engineers Profiles
+      <ul class="list-group">
+      <li class="list-group-item">Name: ${answers.engineerName}</li>
+      <li class="list-group-item">ID: ${answers.engineerId}</li>
+      <li class="list-group-item">Email: ${answers.engineerEmail}</li>
+      <li class="list-group-item">LinkedIn:Github: ${answers.engineerGithub}</li>
+    </ul>
+    </div>
+    <div class="col">
+      Interns Profiles
+      <ul class="list-group">
+      <li class="list-group-item">Name: ${answers.internName}</li>
+      <li class="list-group-item">ID: ${answers.internId}</li>
+      <li class="list-group-item">Email: ${answers.internEmail}</li>
+      <li class="list-group-item">School: ${answers.internSchool}</li>
+    </ul>
+    </div>
+  </div>
+</div>
+</body>
+</html>`;
+
 
 
 function mainMenu(){
@@ -13,7 +62,7 @@ function mainMenu(){
             type: "list",
             name: "memberChoice",
             message: "Who would you like to add?",
-            choices: ["Add Engineer", "Add Intern,", "Add Manager", "Quit"]
+            choices: ["Add Engineer", "Add Intern,", "Add Manager", "None, Save and exit!"]
         }
     ]).then(answers =>{
         if(answers.memberChoice === "Add Engineer"){ function createEngineer(){
@@ -40,7 +89,7 @@ function mainMenu(){
                     message: "What is the team engineer's Github?"
                 },
             ]).then(answers =>{
-                const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.Github)
+                const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
                 team.push(engineer)
                 console.log("team", team)
                 console.log(engineer)
@@ -127,37 +176,33 @@ function mainMenu(){
                 
                 createManager()
                
-            } else (answers.memberChoice === "Quit") 
-            createTeam
+            } else (answers.memberChoice === "None, Save and exit!"); {function createTeam(){
+                   
+                ((answers) => {
+                    const htmlPageContent = generateHTML(answers);
+                
+                    fs.writeFile('index.html', htmlPageContent, (err) =>
+                      err ? console.log(err) : console.log('Successfully created index.html!')
+                    );
+                  });
+                
+            } 
+
+            }
+            
+            createTeam()
+            
+            
         })   
     
 }
            
-         
     
 
    
-function createTeam(){
-
-   
-
-    
-} 
 
 
-
-
-
-
-
-    
-            
-
-
-    
-
-
-mainMenu()
+    mainMenu()
 
     
   
